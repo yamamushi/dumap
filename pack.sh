061c8e767950d59bfe6f67b46c8e42288ab71fb0
yamamushi <yamamushi@gmail.com>
2019-07-03 13:04:23 -0500

    Adjusted packer script

--- pack.sh
+++ pack.sh
@@ -1,4 +1,19 @@
 #!/bin/bash
 
-rm du-map.zip
-zip -r du-map.zip ./* -x "pack.sh" -x "du-map.zip" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "index.php" -x "config.php.example"
+while getopts ":rp" opt; do
+  case ${opt} in
+    r ) # release (public zip)
+        rm du-map.zip
+        zip -r du-map.zip ./* -x "pack.sh" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "index.php" -x "config.php.example"
+        exit 0
+      ;;
+    p ) # production (Website)
+        rm du-map.zip
+        zip -r du-map.zip ./* -x "pack.sh" -x "README.md" -x ".*" -x "config.php" -x "TODO" -x "*.html" -x "config.php.example"
+        exit 0
+      ;;
+    \? ) echo "Usage: pack.sh [-r] [-p]"
+      ;;
+  esac
+done
+
