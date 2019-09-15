5783bdbbcb5b8c286a8f875c73d4c2463dd4126d
yamamushi <yamamushi@gmail.com>
2019-09-15 17:28:15 -0400

    Corrected typo in sutime calculator

--- js/sutime.js
+++ js/sutime.js
@@ -37,7 +37,7 @@ function secondsToDhms(seconds) {
 
     var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
     var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " sdminutes, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
     var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
     return dDisplay + hDisplay + mDisplay + sDisplay;
 }
